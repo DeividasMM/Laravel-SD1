114c90f64c114379039feeb9741a4667dc397426
DeividasMM <deividas.miliauskas2013@gmail.com>
2026-03-27 16:04:59 +0200

    Connect CRUD operations to database via Eloquent

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -2,98 +2,27 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Conference;
 
 class EmployeeController extends Controller
 {
     public function index()
     {
-        $conferences = [
-            [
-                'id' => 1,
-                'title' => 'Web Technologijų Konferencija 2026',
-                'date' => '2026-05-15',
-                'time' => '10:00',
-                'address' => 'Vilnius Tech, Sauletekio al. 11',
-                'description' => 'Metinė konferencija apie šiuolaikines web kūrimo technologijas ir praktikas.',
-                'lecturers' => 'Jonas Jonaitis, Petras Petraitis'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Duomenų Analizės ir Technologijų Konferencija',
-                'date' => '2026-06-20',
-                'time' => '09:00',
-                'address' => 'Kauno Technologijos Universitetas',
-                'description' => 'Konferencija skirta naujausių duomenų analizės ir technologijų sprendimų aptarimui.',
-                'lecturers' => 'Dr. Tomas Kazlauskas'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Kibernetinio Saugumo Simpoziumas',
-                'date' => '2026-07-10',
-                'time' => '11:00',
-                'address' => 'Vilnius, Gedimino pr. 50',
-                'description' => 'Diskusijos apie dabartines kibernetinio saugumo grėsmes ir apsaugos metodus.',
-                'lecturers' => 'Rūta Kazlauskienė, Darius Kavaliauskas'
-            ]
-        ];
+        $conferences = Conference::all();
 
         return view('employee.conference-list', compact('conferences'));
     }
 
     public function show($id)
     {
-        $conferences = [
-            1 => [
-                'id' => 1,
-                'title' => 'Web Technologijų Konferencija 2026',
-                'date' => '2026-05-15',
-                'time' => '10:00',
-                'address' => 'Vilnius Tech, Sauletekio al. 11',
-                'description' => 'Metinė konferencija apie šiuolaikines web kūrimo technologijas ir praktikas.',
-                'lecturers' => 'Jonas Jonaitis, Petras Petraitis',
-                'registered_clients' => [
-                    ['name' => 'Ona Petraitienė', 'email' => 'ona.petraitiene@gmail.com'],
-                    ['name' => 'Jonas Petrauskas', 'email' => 'jonas.petrauskas@gmail.com'],
-                    ['name' => 'Marija Kazlauskienė', 'email' => 'marija.kazlauskiene@gmail.com']
-                ]
-            ],
-            2 => [
-                'id' => 2,
-                'title' => 'Duomenų Analizės ir Technologijų Konferencija',
-                'date' => '2026-06-20',
-                'time' => '09:00',
-                'address' => 'Kauno Technologijos Universitetas',
-                'description' => 'Konferencija skirta naujausių duomenų analizės ir technologijų sprendimų aptarimui.',
-                'lecturers' => 'Dr. Tomas Kazlauskas',
-                'registered_clients' => [
-                    ['name' => 'Tomas Vasiliauskas', 'email' => 'tomas.vasiliauskas@gmail.com'],
-                    ['name' => 'Elena Butkienė', 'email' => 'elena.butkiene@gmail.com']
-                ]
-            ],
-            3 => [
-                'id' => 3,
-                'title' => 'Kibernetinio Saugumo Simpoziumas',
-                'date' => '2026-07-10',
-                'time' => '11:00',
-                'address' => 'Vilnius, Gedimino pr. 50',
-                'description' => 'Diskusijos apie dabartines kibernetinio saugumo grėsmes ir apsaugos metodus.',
-                'lecturers' => 'Rūta Kazlauskienė, Darius Kavaliauskas',
-                'registered_clients' => [
-                    ['name' => 'Petras Jankauskas', 'email' => 'petras.jankauskas@gmail.com'],
-                    ['name' => 'Greta Lukaitė', 'email' => 'greta.lukaite@gmail.com'],
-                    ['name' => 'Andrius Kazlauskas', 'email' => 'andrius.kazlauskas@gmail.com'],
-                    ['name' => 'Ieva Stonytė', 'email' => 'ieva.stonyte@gmail.com']
-                ]
-            ]
-        ];
-
-        $conference = $conferences[$id] ?? null;
+        $conference = Conference::find($id);
 
         if (!$conference) {
             abort(404);
         }
 
-        return view('employee.conference-details', compact('conference'));
+        $registeredClients = $conference->users;
+
+        return view('employee.conference-details', compact('conference', 'registeredClients'));
     }
 }
--- resources/views/employee/conference-details.blade.php
+++ resources/views/employee/conference-details.blade.php
@@ -45,7 +45,7 @@
                     <hr>
                 </div>
 
-                @if(count($conference['registered_clients']) > 0)
+                @if(count($registeredClients) > 0)
                 <div class="table-responsive">
                     <table class="table table-striped">
                         <thead>
@@ -55,10 +55,10 @@
                             </tr>
                         </thead>
                         <tbody>
-                            @foreach($conference['registered_clients'] as $client)
+                            @foreach($registeredClients as $client)
                             <tr>
-                                <td>{{ $client['name'] }}</td>
-                                <td>{{ $client['email'] }}</td>
+                                <td>{{ $client->name }} {{ $client->surname }}</td>
+                                <td>{{ $client->email }}</td>
                             </tr>
                             @endforeach
                         </tbody>
